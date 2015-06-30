c4131b001f43215ec83e9978bda6a54b03007cbd
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-06-30 02:36:53 +0200

    adding predictive modeling for adaptive scheduling ...

---
+++ demo/explore-hog-image-bs-freq/model-sklearn-dtc-validate-ds3.bat
@@ -0,0 +1,1 @@
+ck validate model: @model-input.json @model-input-visualize.json @model-sklearn-dtc-ds3.json > model-sklearn-dtc-ds3-validate.txt
---
+++ demo/explore-hog-image-bs-freq/model-sklearn-dtc-validate-ds4.bat
@@ -0,0 +1,1 @@
+ck validate model: @model-input.json @model-input-visualize.json @model-sklearn-dtc-ds4.json > model-sklearn-dtc-ds4-validate.txt
