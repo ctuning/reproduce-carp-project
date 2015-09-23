ddf6748248bdbd10798a2e1d5cafcf8430e06421
Anton Lokhmotov <anton@dividiti.com>
2015-09-23 11:09:21 +0000

    Using submodules magic in .sh; TODO .bat

--- program/polyhedral-hog-opencl/get_from_git.bat
+++ program/polyhedral-hog-opencl/get_from_git.bat
@@ -1,1 +1,2 @@
+# TODO: see 'get_from_git.sh'.
 git clone https://github.com/rbaghdadi/extended-polyhedral-benchmark extended-polyhedral-benchmark
--- program/polyhedral-hog-opencl/get_from_git.sh
+++ program/polyhedral-hog-opencl/get_from_git.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-git clone https://github.com/rbaghdadi/extended-polyhedral-benchmark extended-polyhedral-benchmark
+
+echo "Updating submodule 'extended-polyhedral-benchmark'..."
+cd ../../
+git submodule init
+git submodule update
+
+echo "Patching submodule 'extended-polyhedral-benchmark'..."
+cd program/polyhedral-hog-opencl/ && ./apply_patch.sh
+
+echo "To revert patch, use 'revert_patch.sh'..."
